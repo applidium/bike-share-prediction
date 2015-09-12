63e8c154864a5935371345d48fd5bc547d46233d
Flavien Raynaud <flavien.raynaud@gmail.com>
2015-09-12 16:05:07 +0200

    API keys can be set via environment variables, suffixed with _APIKEY

--- config/initializers/api_keys.rb
+++ config/initializers/api_keys.rb
@@ -1,8 +1,18 @@
 module ApiKeys
-  @config = YAML.load_file("#{Rails.root}/config/api_keys.yml").symbolize_keys
+  config_path = "#{Rails.root}/config/api_keys.yml"
+  if File.exists?(config_path)
+    @config = YAML.load_file(config_path).symbolize_keys
 
-  # define JCDECAUX & OPENWEATHERMAP constants
-  @config.each do |key, value|
-    const_set key.upcase, value
+    # define JCDECAUX, OPENWEATHERMAP & FORECASTIO constants
+    @config.each do |key, value|
+      const_set key.upcase, value
+    end
+  end
+
+  # better to run on Heroku
+  ENV.each do |var, value|
+    if var.end_with?("_APIKEY")
+      const_set var[0..-8], value
+    end
   end
 end
